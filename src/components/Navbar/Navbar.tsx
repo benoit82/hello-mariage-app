0101b6d6bed498b9d4432e398e9c8e1179478ea2
Benoit Dubus <benoit.dubus@abtasty.com>
2022-03-01 19:23:41 +0100

    feat(navbar): update
    * styling
    
    * button behavior

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,28 @@
-import { FC, useState } from 'React';
+import { FC, useState } from 'react';
+
 import classNames from 'classnames';
 
 import style from './Navbar.module.scss';
 
 const Navbar: FC = () => {
-  const [displayMenu, setDisplayMenu] = useState(true);
-
-  const navBarBtnHander = () => {
+  const [displayMenu, setDisplayMenu] = useState<boolean>(true);
+  const handleNavBarBtn = () => {
     setDisplayMenu(!displayMenu);
   };
 
   return (
     <div className={style.navbarContainer}>
-      <ul className={classNames(style.linkList)}>
+      <ul
+        className={classNames(style.linkList, {
+          [style.linkDisplay]: displayMenu,
+        })}
+      >
         <li>Link 1</li>
         <li>Link 2</li>
         <li>Link 3</li>
         <li>Link 4</li>
       </ul>
-      <button className={style.navBarBtn} onClick={navBarBtnHander}>
+      <button className={style.navBarBtn} onClick={handleNavBarBtn}>
         Menu
       </button>
     </div>

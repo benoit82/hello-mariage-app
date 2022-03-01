99fc98a1ad0302abe0846029457eb7079e288e89
Benoit Dubus <benoit.dubus@abtasty.com>
2022-03-01 17:56:35 +0100

    feat(navbar): edit navbar
    * add classnames dependency
    
    * add story to storybook
    
    * update style

---
+++ __stories__/Navbar.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../src/components/Navbar';
+import { ComponentMeta } from '@storybook/react';
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Navbar',
+  component: Navbar,
+} as ComponentMeta<typeof Navbar>;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,26 @@
-import { FC } from 'React';
+import { FC, useState } from 'React';
+import classNames from 'classnames';
 
 import style from './Navbar.module.scss';
 
 const Navbar: FC = () => {
-  const link: string = 'link';
+  const [displayMenu, setDisplayMenu] = useState(true);
+
+  const navBarBtnHander = () => {
+    setDisplayMenu(!displayMenu);
+  };
 
   return (
     <div className={style.navbarContainer}>
-      <ul>
-        <li>{link} 1</li>
-        <li>{link} 2</li>
+      <ul className={classNames(style.linkList)}>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+        <li>Link 4</li>
       </ul>
+      <button className={style.navBarBtn} onClick={navBarBtnHander}>
+        Menu
+      </button>
     </div>
   );
 };

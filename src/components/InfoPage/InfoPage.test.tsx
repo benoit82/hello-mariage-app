e4c9b396df33f58efbe9ce8b63feff17d59471c2
Benoit Dubus <benoit.dubus@abtasty.com>
2022-03-29 17:32:50 +0200

    test(InfoPage): add tests for component
    test by importing an JSX Element
    
    test by custom JSX Element

---
+++ src/components/InfoPage/InfoPage.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import cgu from '../../informations/cgu';
+import InfoPage from '.';
+
+describe('InfoPage', () => {
+  it('renders an information page like CGU page, by passing informations as children', () => {
+    render(<InfoPage>{cgu}</InfoPage>);
+
+    const cguContent = screen.getByRole('heading', {  name: /cgu \- condition général d'utilisation/i});
+    expect(cguContent).toBeInTheDocument();
+  });
+
+  it('renders informations containing in a JSX Element as children', () => {
+    const myCustomJSXElement: JSX.Element = <p>my test should pass</p>;
+    render(<InfoPage>{myCustomJSXElement}</InfoPage>);
+
+    const customText = screen.getByText(/my test should pass/i);
+    expect(customText).toBeInTheDocument();
+  });
+});

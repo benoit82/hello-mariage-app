f75ec9607c2db1613a4f9db8f1968634c5191b3e
Benoit Dubus <benoit.dubus@abtasty.com>
2022-03-29 16:39:00 +0200

    feat(homepage): add homepage content
    add title + description of the page
    
    add css rules
    
    remove vercel icon

--- __testsPages__/index.test.tsx
+++ __testsPages__/index.test.tsx
@@ -5,9 +5,15 @@ describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />);
 
-    const homepageTitle = screen.getByRole('heading', {  name: /mariage à thème et sur mesure/i});
-    const homepageDescription = screen.getByText(/construisons ensemble votre projet/i);
+    const homepageTitle = screen.getByRole('heading', {
+      name: /mariage à thème et sur mesure/i,
+    });
+    const homepageDescription = screen.getByText(
+      /construisons ensemble votre projet/i,
+    );
 
-    [homepageTitle, homepageDescription].forEach(element => expect(element).toBeInTheDocument());
+    [homepageTitle, homepageDescription].forEach((element) =>
+      expect(element).toBeInTheDocument(),
+    );
   });
 });

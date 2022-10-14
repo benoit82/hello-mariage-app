3fea2204450e64476325b79b7dd6cdfb5c0aa1a6
Benoit Dubus <bntdbs@gmail.com>
2022-10-14 14:22:02 +0200

    Feature/image (#5)
    * add image to test
    
    * update
    
    * update
    
    * add youtube video exemple
    
    * remove image nature
    
    Co-authored-by: Benoit Dubus <benoit.dubus@abtasty.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,15 @@ const Home: NextPage = () => {
 
       <h1>Mariage à thème et sur mesure</h1>
       <p className={styles.description}>Construisons ensemble votre projet</p>
+      <div className="youtube-iframe-video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/9IOBFl5SQVM?controls=0"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+      </div>
     </div>
   );
 };

4206cadc3dc69a88a1bea2ea312ab088fe34177e
Benoit Dubus <benoit.dubus@abtasty.com>
2024-06-10 17:14:10 +0200

    chore(YT video): change video link

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/9IOBFl5SQVM?controls=0"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         ></iframe>
